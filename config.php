0320c89b6574aab05a79d0f15ca7a6ae02dbccd3
Eric Chang <changey@mit.edu>
2014-03-17 21:47:52 -0700

    setup

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $mysql_password = "";*/
 $mysql_hostname = "127.0.0.1";
 $mysql_user = "root";
 //$mysql_password = "address";
-$mysql_database = "social";
+$mysql_database = "pik";
 $con = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) 
 or die("Can't connect to database");
 mysql_select_db($mysql_database, $con) or die("Can't select database");
